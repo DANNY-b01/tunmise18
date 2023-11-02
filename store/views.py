f8643f3b5baf8760d643733521d9cd2455b99cc5
DANNY-b01 <dmuibi5@gmail.com>
2023-11-02 14:32:07 +0100

    updates

--- store/views.py
+++ store/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .forms import productiom
+from .models import product
 
 
 
 
 def homepage(request):
-    return render(request, "store/index.html")
+    all_products=product.objects.all()
+    context={"all_products": all_products}
+    return render(request, "store/index.html", context)
+def detailpage(request, input_id):
+    current_product = product.objects.get(id=input_id)
+    context={"current_product": current_product}
+    return render(request, "store/detail.html", context)
+
+
     
     # return HttpResponse("<h1> Home page")
 
